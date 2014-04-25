46e84b35ef0c39f7bdc2df101cd6c48b97ab7df9
Yukun Lin <me@yukun.io>
2014-04-25 12:49:34 -0700

    mysterious array

--- pendulum.py
+++ pendulum.py
@@ -36,5 +36,8 @@ class InvertedPendulum:
         self.control = controlInput
         self.time += self.DT
         self.translational[1] = (controlInput / self.MASS) * self.DT + self.translational[1]
+        print (controlInput / self.MASS) * self.DT + self.translational[1]
         self.translational[0] = self.translational[1] * self.DT + self.translational[0]
         self.rotational = self.rungeKutta(self.rotational)
+
+ip = InvertedPendulum()
